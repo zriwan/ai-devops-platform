ce5939901abcc7343cb9dd120be059e536f5c81d
Zahaib Bin Rizwan <fa22-bcs-132@cuivehari.edu.pk>
2026-07-13 17:46:12 +0500

    Add Prometheus Grafana and container monitoring

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,22 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
