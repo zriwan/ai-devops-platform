bc3583e81cc30fd2af7168e52db44e1ab2a03d9a
Zahaib Bin Rizwan <fa22-bcs-132@cuivehari.edu.pk>
2026-07-13 16:47:02 +0500

    Deploy Nginx reverse proxy with Docker Compose

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
